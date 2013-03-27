47e5a5c0352d8bb46999a396b4eb256af749add6
Ilya Beda <ir4y.ix@gmail.com>
2013-03-27 21:01:55 +0800

    Add directory walker

--- src/clojure_course_task02/core.clj
+++ src/clojure_course_task02/core.clj
@@ -1,6 +1,18 @@
 (ns clojure-course-task02.core
   (:gen-class))
 
+(require '[clojure.java.io :as io])
+
+(defn to-path [path file]
+  (apply str path "/" file))
+
+(defn get-file-list [path]
+  (let [full-list (-> (io/file path) .list vec)
+        file-list (filter #(-> (io/file %) .isDirectory not) full-list)
+        dir-list (filter #(-> (io/file %) .isDirectory) full-list)]
+    (if (empty? dir-list)
+      file-list
+      (mapcat #(get-file-list (to-path path %)) dir-list))))
 
 (defn find-files [file-name path]
   "TODO: Implement searching for a file using his name as a regexp."
